59f4f6bfa0d1ddd05fe802a785cb0165d0c10599
lindomar corsino <lindomarc@outlook.com>
2019-06-15 14:54:42 -0300

    Ajax com MVC

---
+++ App/Controllers/Site/NewsController.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Controllers\Site;
+use \App\Controllers\BaseController as BaseController;
+use \App\Models\Site\News;
+
+class NewsController extends BaseController{
+
+    
+    public function listNews(){
+
+        /**
+         * Listar noticias
+         */
+        $news = News::list('all');
+        $news_array = [];
+        foreach ($news as $new) {
+            $news_array[]= $new->to_array();
+        }
+
+        echo json_encode( $news_array );
+
+    }
+}
