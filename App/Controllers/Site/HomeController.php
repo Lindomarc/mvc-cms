8b32e8d98406917a3516abf61b521a5a30bcdeb0
lindomar corsino <lindomarc@outlook.com>
2019-06-20 18:47:19 -0300

    insert pdo

--- App/Controllers/Site/HomeController.php
+++ App/Controllers/Site/HomeController.php
@@ -15,4 +15,5 @@ class HomeController extends ContainerController
     }
 
 
+
 }
--- App/Models/Model.php
+++ App/Models/Model.php
@@ -2,7 +2,9 @@
 namespace App\Models;
 use \App\Models\Connection;
 use \App\Traits\CollectionDb;
-// use \App\Traits\PersistDb;
+use \App\Traits\PersistDb;
+use \App\Classes\App;
+
 
 abstract class Model 
 {   
@@ -10,12 +12,15 @@ abstract class Model
      * Collection  - select
      * Persist - Registrando, Atualizando.
      */
-    use CollectionDb;
+    use CollectionDb, PersistDb;
 
     public $connection;
 
     public function __construct() {
+        
         $this->connection = Connection::connection();
+
+        App::bind('connection',$this->connection);
     }
 
 }
